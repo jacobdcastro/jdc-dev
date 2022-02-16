ade1ef46d05ae30c7acacb1afe8dd778cc7db9ff
Jacob D. Castro <jacob@jdc.dev>
2022-02-15 19:50:15 -0700

    minor edits

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,39 +5,43 @@ import PurpleBg from 'components/PurpleBg';
 import SocialLinks from 'components/SocialLinks';
 import { shuffleArray, skills } from 'lib/skills';
 import MobileNav from 'components/MobileNav';
+import Head from 'components/Head';
 
 const HomePage = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="relative">
-      <PurpleBg />
+    <>
+      <Head />
+      <div className="relative">
+        <PurpleBg />
 
-      <MobileNav open={open} setOpen={setOpen} />
+        <MobileNav open={open} setOpen={setOpen} />
 
-      <div className="absolute h-screen w-screen bg-transparent z-20">
-        <nav className="w-screen fixed flex justify-center z-30">
-          <Hamburger onClick={() => setOpen(true)} />
-        </nav>
+        <div className="absolute h-screen w-screen bg-transparent z-20">
+          <nav className="w-screen fixed flex justify-center z-30">
+            <Hamburger onClick={() => setOpen(true)} />
+          </nav>
 
-        <MainView />
+          <MainView />
 
-        <SocialLinks />
+          <SocialLinks />
 
-        <button className="ticker-wrap cursor-pointer">
-          <div className="ticker">
-            {shuffleArray(skills).map(item => (
-              <div
-                key={item}
-                className="ticker-item text-white inline-block font-medium text-md whitespace-nowrap monument pr-24"
-              >
-                {item}
-              </div>
-            ))}
-          </div>
-        </button>
+          <button className="ticker-wrap cursor-pointer">
+            <div className="ticker">
+              {shuffleArray(skills).map(item => (
+                <div
+                  key={item}
+                  className="ticker-item text-white inline-block font-medium text-md whitespace-nowrap monument pr-24"
+                >
+                  {item}
+                </div>
+              ))}
+            </div>
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
